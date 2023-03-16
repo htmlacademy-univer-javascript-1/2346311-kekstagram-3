e83462932cad148f376b6ace17237f5ded1f1a93
Sana <amalsk0905@yandex.ru>
2023-03-16 15:27:59 +0300

    Модуляция

--- js/data.js
+++ js/data.js
@@ -1,18 +1,17 @@
 import { getRandomInt } from './util.js';
 const photos = [];
 
-function getPhotos() {
-  for (let i = 0; i < 25; i++) {
-    const newElem = {
-      id: i+1,
-      url: `photos/${i+1}.jpg`,
-      description: `Ссылка: ${photos[i].url}, номер: ${photos[i].id}`,
-      likes: getRandomInt(15,200),
-      comments: getRandomInt(0,200)
-    };
-    photos.push(newElem);
-  }
-  return photos;
+
+for (let i = 0; i < 25; i++) {
+  const newElem = {
+    id: i+1,
+    url: `photos/${i+1}.jpg`,
+    description: `Ссылка: ${photos[i].url}, номер: ${photos[i].id}`,
+    likes: getRandomInt(15,200),
+    comments: getRandomInt(0,200)
+  };
+  photos.push(newElem);
 }
+
 
 export {photos};
