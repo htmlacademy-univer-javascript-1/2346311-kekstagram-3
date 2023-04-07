051e37b98a44cf82a2af6281f2a508f4f8b56b32
Sana <amalsk0905@yandex.ru>
2023-04-07 14:59:18 +0300

    fix

--- js/form.js
+++ js/form.js
@@ -30,3 +30,5 @@ function openImgUpload() {
 
 fileInput.addEventListener('change', openImgUpload);
 closeImgUploadButton.addEventListener('click', closeImgUpload);
+
+export {closeImgUpload};
--- js/formValidation.js
+++ js/formValidation.js
@@ -1,7 +1,11 @@
-import {checkLength} from './util.js';
+import { closeImgUpload } from './form.js';
+import { checkLength } from './util.js';
 
-const hashtagPattern = new RegExp('^#[а-яА-ЯёЁa-zA-Z0-9]{1-19}$');
+const hashtagRegex = /\W(#[а-яa-z]+\b)/gmi;
 const form = document.querySelector('img-upload__form');
+const isHashtagValid = (value) => checkLength(value, 20) && !checkLength(value, 2) && !hashtagRegex.test(value);
+
+
 const valid = new Pristine(form, {
   classTo: 'img-upload__text',
   successClass: 'img-upload__text--valid',
@@ -12,18 +16,15 @@ const valid = new Pristine(form, {
 });
 
 
-form.addEventListener('submit', (evt) => {
-  if (!valid.validate()) {
-    evt.preventDefault();
-  }
-});
-
-
-valid.addValidator(form.querySelector('.text__hashtags'),
-  (value) => !checkLength(value, 0) && !hashtagPattern.test(value),
+valid.addValidator(
+  form.querySelector('.text__hashtags'),
+  isHashtagValid,
   'Формат хэштега: #anySymbols(<=20)');
 
-valid.addValidator(form.querySelector('.text__description'),
-  (value) => checkLength(value, 19) || !checkLength(value, 140),
-  'Длина описания может быть от 20 до 140 символов'
-);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  if (!valid.validate()) {
+    closeImgUpload();
+  }
+});
